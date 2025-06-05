41185d412be03006a07906d452b2751029f08b36
Junlong Li <45759388+lockon-n@users.noreply.github.com>
2025-06-05 18:36:29 +0800

    Update google_scholar.py

--- src/mcp_scholarly/google_scholar.py
+++ src/mcp_scholarly/google_scholar.py
@@ -17,13 +17,14 @@ class GoogleScholar:
         results_iter = 0
         for searched_article in search_results:
             bib = searched_article.get('bib', {})
-            title = bib.get('title', None)
-            abstract = bib.get('abstract', None)
-            pub_url = searched_article.get('pub_url', None)
-            article_string = "\n".join([title, abstract, pub_url])
+            title = bib.get('title', 'No title')
+            abstract = bib.get('abstract', 'No abstract available')
+            pub_url = searched_article.get('pub_url', 'No URL available')
+            
+            article_string = f"Title: {title}\nAbstract: {abstract}\nURL: {pub_url}"
             articles.append(article_string)
             results_iter += 1
-            if results_iter > MAX_RESULTS:
+            if results_iter >= MAX_RESULTS:
                 break
         return articles
 
